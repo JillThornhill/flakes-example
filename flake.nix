78328620f51eea5f40627c8236155d8fec21b94e
Jill <jill.thornhill@zol.co.zw>
2023-01-06 15:03:16 +0200

    Added SSH key

--- flake.nix
+++ flake.nix
@@ -24,10 +24,8 @@
           };
           services.openssh.enable = true;
 
-          users.users.root.openssh.authorizedKeys.keys = [
-            # change this to your ssh key
-            "CHANGE"
-          ];
+          users.users.root.openssh.authorizedKeys.keys = [ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFwvjIb51SQ5ISetVMQWkGwNXmJeZzeOMGZaugfwGgY jill@ubuntu-virtual-machine
+];
         })
       ];
     };
