258eea7e0908512cdebd92c642e1e234796269c8
Jill <jill.thornhill@zol.co.zw>
2023-01-07 15:43:39 +0200

    Fixed format or authorized user line

--- flake.nix
+++ flake.nix
@@ -24,8 +24,7 @@
           };
           services.openssh.enable = true;
 
-          users.users.root.openssh.authorizedKeys.keys = [ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFwvjIb51SQ5ISetVMQWkGwNXmJeZzeOMGZaugfwGgY jill@ubuntu-virtual-machine
-];
+          users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFwvjIb51SQ5ISetVMQWkGwNXmJeZzeOMGZaugfwGgY jill@ubuntu-virtual-machine" ];
         })
       ];
     };
